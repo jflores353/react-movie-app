b460b50bbcac27d80412b9c499b871bf7581d0f1
Gabe <jflores353@gmail.com>
2019-11-27 02:13:31 -0600

    added a Link around img to direct towards page with id

--- src/Movie.js
+++ src/Movie.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const POSTER_PATH = 'https://image.tmdb.org/t/p/w154';
+const POSTER_PATH = "https://image.tmdb.org/t/p/w154";
 
-const Movie = ({movie}) => (
+const Movie = ({ movie }) => (
   <div>
-    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    <Link to={`/${movie.id}`}>
+      <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Link>
   </div>
-)
+);
 
 export default Movie;
 
 Movie.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-  }).isRequired,
-}
+    title: PropTypes.string.isRequired
+  }).isRequired
+};
