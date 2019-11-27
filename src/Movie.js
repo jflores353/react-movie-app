7cf4107e59faa8cae4c4cb5c0d87e0bc789b4eea
Gabe <jflores353@gmail.com>
2019-11-26 23:17:02 -0600

    configured eslint, changed Movie.js to functional stateless component. Moved propTypes to bottom. Instead of passing props just passing { movie } since it is the object needed.

--- src/Movie.js
+++ src/Movie.js
@@ -1,19 +1,16 @@
-import React , {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Movie extends Component {
+const Movie = ({movie}) => (
+  <div>
+    <h3>{movie.title}</h3>
+  </div>
+)
 
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-    }),
-  }
-  
-  render(){
-      return(
-        <div>
-          <h3>{this.props.movie.title}</h3>
-        </div>
-      )
-  }
+export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 }
