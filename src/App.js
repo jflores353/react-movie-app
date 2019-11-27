e8a7caa0480878427c51ee1223887711934eed99
Gabe <jflores353@gmail.com>
2019-11-27 02:02:09 -0600

    Installed React router. Made App a functional component. Created home path to be MoviesList.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import './App.css';
 
-import Movie from "./Movie"
+import MoviesList from "./MoviesList"
 
-class App extends Component {
-  state = {
-    movies : [],
-  }
-
-  async componentDidMount(){
-    try{
-      const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=3fd7e8eaad02da945713b598cfa29cba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
-      const movies = await response.json();
-      this.setState({
-        movies : movies.results,
-      })
-    }catch(error){
-      console.log(error)
-    }
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>React Movie App</h1>
-          </header>
-        {this.state.movies.map(film => <Movie key={film.id} movie={film}/>)}
-      </div>
-    );
-  }
-}
-
-
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+      <h1>React Movie App</h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />  
+      </Switch>
+    </div>
+  </Router>
+)
 
 export default App;
+
