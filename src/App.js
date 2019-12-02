e82aaf85c26908ef1b3f5f4671d6cce9cbf12c48
Gabe <jflores353@gmail.com>
2019-12-01 23:03:43 -0600

    changed header text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
 
-import MoviesList from "./MoviesList"
-import MovieDetail from "./MovieDetail"
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
 
 const App = () => (
   <Router>
     <div className="App">
       <header className="App-header">
         <Link to="/">
-          <h1>React Movie App</h1>
+          <h1>REACT MOVIE APP</h1>
         </Link>
       </header>
       <Switch>
-        <Route exact path="/" component={MoviesList} />  
-        <Route exact path="/:id" component={ MovieDetail } />  
+        <Route exact path="/" component={MoviesList} />
+        <Route exact path="/:id" component={MovieDetail} />
       </Switch>
     </div>
   </Router>
-)
+);
 
 export default App;
-
