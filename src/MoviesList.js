7a76467fe283dd9d84ffb3482524dc55be426925
Gabe <jflores353@gmail.com>
2019-12-01 23:10:26 -0600

    Created MovieGrid styled component. Used grid to create layout of poster tiles for landing page.

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,30 +1,42 @@
-import React, {Component} from 'react';
-import Movie from "./Movie"
+import React, { Component } from "react";
+import Movie from "./Movie";
+import styled from "styled-components";
 
 class MoviesList extends Component {
   state = {
-    movies : [],
-  }
+    movies: []
+  };
 
-  async componentDidMount(){
-    try{
-      const response = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=3fd7e8eaad02da945713b598cfa29cba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=3fd7e8eaad02da945713b598cfa29cba&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+      );
       const movies = await response.json();
       this.setState({
-        movies : movies.results,
-      })
-    }catch(error){
-      console.log(error)
+        movies: movies.results
+      });
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  render(){
+  render() {
     return (
-      <div>
-          {this.state.movies.map(film => <Movie key={film.id} movie={film}/>)} 
-      </div>
+      <MovieGrid>
+        {this.state.movies.map(movie => (
+          <Movie key={movie.id} movie={movie} />
+        ))}
+      </MovieGrid>
     );
   }
 }
 
 export default MoviesList;
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1 rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1 rem;
+`;
